4a2f00fdae94b27c98b5299f5d47c8511d304430
Loneastro95 <ayandasontlaba6@gmail.com>
2023-10-30 13:59:02 +0200

    first commit

--- App.js
+++ App.js
@@ -1,12 +1,23 @@
+import { Camera } from 'expo-camera';
 import { StatusBar } from 'expo-status-bar';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { StyleSheet, Text, View } from 'react-native';
+import Cameras from './Component/Camera';
+import Cam from './Component/Cam';
+import Gallery from './Component/Gallery';
 
+
+
+const Stack = createNativeStackNavigator();
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName='Cam' >
+        <Stack.Screen name="Cam" component={Cam} />
+        <Stack.Screen name="Gallery" component={Gallery} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
---
+++ Component/takepic.js
@@ -0,0 +1,13 @@
+import { Text, View } from 'react-native'
+import React, { Component } from 'react'
+const takepic = ()  => {
+
+    return (
+      <View>
+        <Text>takepic</Text>
+      </View>
+    )
+  }
+
+
+export default takepic
